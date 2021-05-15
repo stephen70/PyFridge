54bb5a3db036f0fd5e2d4b213b02f15505176276
Stephen Ryan <stephenryan70@gmail.com>
2021-05-15 19:20:09 +0100

    df

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import numpy
 import pandas
 from PIL import Image
 import math
+import itertools
 
 class MagnetBlock(object):
     def __init__(self, color, hexcolor, number):
@@ -27,31 +28,43 @@ blocks = [
 
 block_colors = [block.hexcolor for block in blocks]
 
+def rgb_to_hex(rgb):
+    hex = f'{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+    return hex
+
+def hex_to_rgb(hex):
+    r = int(hex[0:2], 16) # red color value
+    g = int(hex[2:4], 16) # green color value
+    b = int(hex[4:6], 16) # blue color value
+    return (r, g, b)
 
 def color_distance(color1, color2):
-    r1 = int(color1[0:2], 16) # red color value
-    g1 = int(color1[2:4], 16) # green color value
-    b1 = int(color1[4:6], 16) # blue color value
-    r2 = int(color2[0:2], 16) # red color value
-    g2 = int(color2[2:4], 16) # green color value
-    b2 = int(color2[4:6], 16) # blue color value
+    r1, g1, b1 = hex_to_rgb(color1)
+    r2, g2, b2 = hex_to_rgb(color2)
 
-    distance = math.sqrt(
-        (r2 - r1) ** 2 +
-        (g2 - g1) ** 2 +
-        (b2 - b1) ** 2)
+    # no need to take square root, for efficiency
 
+    distance = (r2 - r1) ** 2 + (g2 - g1) ** 2 + (b2 - b1) ** 2
     return distance
-
 
 img = Image.open("Images/girl_with_a_pearl_earring.jpg")
 
 # first downsize image for efficiency, wont be using a 1920x1080 image
 # then map each pixel to its nearest neighbour
+img_small = img.resize((100, 100))
 
-for pixel in img:
+width, height = img_small.size
 
-width, height = img.size
+for pixel in itertools.product(range(width), range(height)):
+    print(pixel)
+    rgb = img_small.getpixel(xy=(pixel))
+    hex = rgb_to_hex(rgb)
 
-img_small = img.resize((width//6, height//6))
+    distances = [(color_distance(hex, block_color), block_color) for block_color in block_colors]
+    distances = sorted(distances, key=lambda x: x[0])
+    new_color_hex = distances[0][1]
+    new_color_rgb = hex_to_rgb(new_color_hex)
+
+    img_small.putpixel(xy=(pixel), value=new_color_rgb)
+
 img_small.show()
